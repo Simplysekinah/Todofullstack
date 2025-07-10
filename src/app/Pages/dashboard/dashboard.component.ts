e5301071a1d7858f3a50eb7a4b55497b02e1a2a8
Simplysekinah <akinolasekinat123@gmail.com>
2025-07-10 16:07:13 +0100

    add icon

--- src/app/Pages/dashboard/dashboard.component.ts
+++ src/app/Pages/dashboard/dashboard.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { AuthService } from '../../Service/Auth/auth.service';
 import { Router } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { bootstrapBagPlusFill } from '@ng-icons/bootstrap-icons';
+import { bootstrapBagPlusFill,bootstrapPlusCircleFill } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-dashboard',
   imports: [NgIcon],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
-  viewProviders:[provideIcons({bootstrapBagPlusFill})]
+  viewProviders:[provideIcons({bootstrapBagPlusFill,bootstrapPlusCircleFill})]
 })
 export class DashboardComponent {
   
